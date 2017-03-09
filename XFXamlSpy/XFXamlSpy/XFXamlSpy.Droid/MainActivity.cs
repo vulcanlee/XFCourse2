8f8c5ba1fea3a8e5bb110009398f738d7c139315
李進興 Vulcan <vulcan@miniasp.com>
2017-03-09 19:51:20 +0800

    XAML Spy 的功能展示範例

---
+++ XFXamlSpy/XFXamlSpy/XFXamlSpy.Droid/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+
+namespace XFXamlSpy.Droid
+{
+    [Activity(Label = "XFXamlSpy", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.tabs;
+            ToolbarResource = Resource.Layout.toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+
+            var service = FirstFloor.XamlSpy.Services.XamlSpyService.Current;
+            service.Connect("169.254.80.80", 4530, "98981");
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}
+
---
+++ XFXamlSpy/XFXamlSpy/XFXamlSpy/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,122 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using XFXamlSpy.Models;
+
+namespace XFXamlSpy.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        #region Repositories (遠端或本地資料存取)
+        #endregion
+
+        #region ViewModel Property (用於在 View 中作為綁定之用)
+
+        #region Title
+        private string _Title;
+        /// <summary>
+        /// Title
+        /// </summary>
+        public string Title
+        {
+            get { return this._Title; }
+            set { this.SetProperty(ref this._Title, value); }
+        }
+        #endregion
+        #region DataItemList
+        private ObservableCollection<DataItem> _DataItemList = new ObservableCollection<DataItem>();
+        /// <summary>
+        /// DataItemList
+        /// </summary>
+        public ObservableCollection<DataItem> DataItemList
+        {
+            get { return _DataItemList; }
+            set { SetProperty(ref _DataItemList, value); }
+        }
+        #endregion
+
+        #region IsBusy
+        private bool _IsBusy;
+        /// <summary>
+        /// IsBusy
+        /// </summary>
+        public bool IsBusy
+        {
+            get { return this._IsBusy; }
+            set { this.SetProperty(ref this._IsBusy, value); }
+        }
+        #endregion
+
+        #endregion
+
+        #region Field 欄位
+
+        public DelegateCommand ListView更新資料Command { get; set; }
+
+        #endregion
+
+        #region Constructor 建構式
+        public MainPageViewModel()
+        {
+            ListView更新資料Command = new DelegateCommand(() =>
+            {
+                // 讓 ListView 出現忙碌中的視覺畫面
+                IsBusy = true;
+                Refresh();
+                // 讓 ListView 隱藏忙碌中的視覺畫面
+                IsBusy = false;
+            });
+        }
+        #endregion
+
+        #region Navigation Events (頁面導航事件)
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+
+            Refresh();
+        }
+        #endregion
+
+        #region 設計時期或者執行時期的ViewModel初始化
+        #endregion
+
+        #region 相關事件
+        #endregion
+
+        #region 相關的Command定義
+        #endregion
+
+        #region 其他方法
+
+        /// <summary>
+        /// 更新 ListView 上的資料
+        /// </summary>
+        public void Refresh()
+        {
+            // 清除原有資料
+            DataItemList.Clear();
+            Random fooRM = new Random();
+            // 隨機新增多筆新的資料
+            for (int i = 0; i < fooRM.Next(20, 100); i++)
+            {
+                DataItemList.Add(new DataItem
+                {
+                    DataVale = $"多奇數位創意有限公司 {fooRM.Next(9999)}",
+                });
+            }
+        }
+        #endregion
+
+    }
+}
