975f6c97611d6578711fce2de8a292a25a538d85
進興 李 <vulcan@miniasp.com>
2017-03-17 22:37:08 +0800

    班車時刻表範例

---
+++ XFTimeline/XFTimeline/XFTimeline/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,148 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using XFTimeline.Models;
+
+namespace XFTimeline.ViewModels
+{
+
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        #region Repositories (遠端或本地資料存取)
+
+        #endregion
+
+        #region ViewModel Property (用於在 View 中作為綁定之用)
+        #region Classes
+        private ObservableCollection<ExerciseClass> _Classes;
+        /// <summary>
+        /// Classes
+        /// </summary>
+        public ObservableCollection<ExerciseClass> Classes
+        {
+            get { return _Classes; }
+            set { SetProperty(ref _Classes, value); }
+        }
+        #endregion
+
+        #endregion
+
+        #region Field 欄位
+
+        private readonly INavigationService _navigationService;
+        #endregion
+
+        #region Constructor 建構式
+        public MainPageViewModel(INavigationService navigationService)
+        {
+
+            #region 相依性服務注入的物件
+
+            _navigationService = navigationService;
+            #endregion
+
+            #region 頁面中綁定的命令
+
+            #endregion
+
+            #region 事件聚合器訂閱
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Navigation Events (頁面導航事件)
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            await ViewModelInit();
+        }
+        #endregion
+
+        #region 設計時期或者執行時期的ViewModel初始化
+        #endregion
+
+        #region 相關事件
+        #endregion
+
+        #region 相關的Command定義
+        #endregion
+
+        #region 其他方法
+
+        /// <summary>
+        /// ViewModel 資料初始化
+        /// </summary>
+        /// <returns></returns>
+        private async Task ViewModelInit()
+        {
+            DataFactory();
+            await Task.Delay(100);
+        }
+
+        void DataFactory()
+        {
+
+            Classes = new ObservableCollection<ExerciseClass>
+            {
+                new ExerciseClass
+                {
+                    ClassName = "Yoga",
+                    Instructor = "Maharshi Patanjali",
+                    ClassTime = TodayAt(8,00),
+                },
+                 new ExerciseClass
+                {
+                    ClassName = "ABS + Stretch",
+                    Instructor = "David Hasslehoff",
+                    ClassTime = TodayAt(9,30),
+                },
+                 //new ExerciseClass
+                //{
+                //    ClassName = "Body Sculpt",
+                //    Instructor = "Sadie Terry",
+                //    ClassTime = DateTime.Now.AddHours(3),
+                //},
+                 new ExerciseClass
+                {
+                    ClassName = "Cycle",
+                    Instructor = "Lance Armstrong",
+                    ClassTime = TodayAt(12,00),
+                },
+                 new ExerciseClass
+                {
+                    ClassName = "Aerobics",
+                    Instructor = "Jacky Chan",
+                    ClassTime = TodayAt(15,30),
+                },
+                 new ExerciseClass
+                {
+                    ClassName = "Weights",
+                    Instructor = "Arnold Schwarzenegger",
+                    ClassTime = TodayAt(18,00),
+                    IsLast = true
+                },
+            };
+        }
+
+        DateTime TodayAt(int hour, int minute)
+        {
+            return new DateTime(DateTime.Now.Year,
+                DateTime.Now.Month,
+                DateTime.Now.Day,
+                hour, minute, 0);
+        }
+        #endregion
+
+    }
+}
