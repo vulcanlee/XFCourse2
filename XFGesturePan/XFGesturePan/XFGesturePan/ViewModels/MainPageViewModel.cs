b69679a719d259b8f2b4230b01aa697097af1eae
李進興 Vulcan <vulcan@miniasp.com>
2017-03-23 10:36:11 +0800

    純粹使用 XAML 與手勢操作功能，做出可以選取圖片某個部分的效果

---
+++ XFGesturePan/XFGesturePan/XFGesturePan/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,155 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFGesturePan.ViewModels
+{
+
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        #region Repositories (遠端或本地資料存取)
+
+        #endregion
+
+        #region ViewModel Property (用於在 View 中作為綁定之用)
+
+        #region CenterHeight
+        private GridLength _CenterHeight = new GridLength(200);
+        /// <summary>
+        /// CenterHeight
+        /// </summary>
+        public GridLength CenterHeight
+        {
+            get { return this._CenterHeight; }
+            set { this.SetProperty(ref this._CenterHeight, value); }
+        }
+        #endregion
+
+        #region CenterWidth
+        private GridLength _CenterWidth = new GridLength(200);
+        /// <summary>
+        /// CenterWidth
+        /// </summary>
+        public GridLength CenterWidth
+        {
+            get { return this._CenterWidth; }
+            set { this.SetProperty(ref this._CenterWidth, value); }
+        }
+        #endregion
+
+        #region RowTop
+        private GridLength _RowTop = new GridLength(1, GridUnitType.Star);
+        /// <summary>
+        /// RowTop
+        /// </summary>
+        public GridLength RowTop
+        {
+            get { return this._RowTop; }
+            set { this.SetProperty(ref this._RowTop, value); }
+        }
+        #endregion
+
+        #region RowBottom
+        private GridLength _RowBottom = new GridLength(1, GridUnitType.Star);
+        /// <summary>
+        /// RowBottom
+        /// </summary>
+        public GridLength RowBottom
+        {
+            get { return this._RowBottom; }
+            set { this.SetProperty(ref this._RowBottom, value); }
+        }
+        #endregion
+
+        #region ColumnLeft
+        private GridLength _ColumnLeft = new GridLength(1, GridUnitType.Star);
+        /// <summary>
+        /// ColumnLeft
+        /// </summary>
+        public GridLength ColumnLeft
+        {
+            get { return this._ColumnLeft; }
+            set { this.SetProperty(ref this._ColumnLeft, value); }
+        }
+        #endregion
+
+        #region ColumnRight
+        private GridLength _ColumnRight = new GridLength(1, GridUnitType.Star);
+        /// <summary>
+        /// ColumnRight
+        /// </summary>
+        public GridLength ColumnRight
+        {
+            get { return this._ColumnRight; }
+            set { this.SetProperty(ref this._ColumnRight, value); }
+        }
+        #endregion
+
+
+        #endregion
+
+        #region Field 欄位
+
+        private readonly INavigationService _navigationService;
+        #endregion
+
+        #region Constructor 建構式
+        public MainPageViewModel(INavigationService navigationService)
+        {
+
+            #region 相依性服務注入的物件
+
+            _navigationService = navigationService;
+            #endregion
+
+            #region 頁面中綁定的命令
+
+            #endregion
+
+            #region 事件聚合器訂閱
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Navigation Events (頁面導航事件)
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            await ViewModelInit();
+        }
+        #endregion
+
+        #region 設計時期或者執行時期的ViewModel初始化
+        #endregion
+
+        #region 相關事件
+        #endregion
+
+        #region 相關的Command定義
+        #endregion
+
+        #region 其他方法
+
+        /// <summary>
+        /// ViewModel 資料初始化
+        /// </summary>
+        /// <returns></returns>
+        private async Task ViewModelInit()
+        {
+            await Task.Delay(100);
+        }
+        #endregion
+
+    }
+}
---
+++ XFGesturePan/XFGesturePan/XFGesturePan/Views/MainPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFGesturePan.ViewModels;
+
+namespace XFGesturePan.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        /// <summary>
+        /// 這個頁面會用到的 ViewModel
+        /// </summary>
+        MainPageViewModel fooMainPageViewModel;
+
+        // 定義各種運算用到的變數
+        double ImageHeight, ImageWidth, OffsetTotal_dx, OffsetTotal_dy, OffsetMoving_dx, OffsetMoving_dy;
+        double OriginalHeight, OriginWidth;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // 取得這個 View 使用的 ViewModel
+            fooMainPageViewModel = this.BindingContext as MainPageViewModel;
+
+            #region 設定要要擷取或者可以看到的圖片範圍大小
+            fooMainPageViewModel.CenterHeight = new GridLength(200);
+            fooMainPageViewModel.CenterWidth = new GridLength(200);
+            #endregion
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await Task.Delay(500);
+            // 取得現在顯示圖片的設計尺寸高度與寬度
+            ImageWidth = PhotoImage.Width;
+            ImageHeight = PhotoImage.Height;
+            Init();
+        }
+
+        private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    //將手勢操作的偏移量先歸零
+                    OffsetMoving_dx = 0;
+                    OffsetMoving_dy = 0;
+                    break;
+
+                case GestureStatus.Running:
+                    // 取得正在手勢操作的偏移量
+                    OffsetMoving_dx = e.TotalX;
+                    OffsetMoving_dy = e.TotalY;
+                    Refresh();
+                    break;
+
+                case GestureStatus.Canceled:
+                    // 操作取消，剛剛的手勢操作，歸零
+                    OffsetMoving_dx = 0;
+                    OffsetMoving_dy = 0;
+                    // 依據偏移量，計算出 Grid 的 Row / Column 的實際需要高度與寬度
+                    Refresh();
+                    break;
+
+                case GestureStatus.Completed:
+                    // 手勢操作完成，將此次手勢操作的位置偏移量，加總到實際位置偏移量中
+                    OffsetTotal_dx += OffsetMoving_dx;
+                    OffsetTotal_dy += OffsetMoving_dy;
+                    // 將手勢位置偏移量歸零
+                    OffsetMoving_dx = 0;
+                    OffsetMoving_dy = 0;
+                    // 依據偏移量，計算出 Grid 的 Row / Column 的實際需要高度與寬度
+                    Refresh();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 依據偏移量，計算出 Grid 的 Row / Column 的實際需要高度與寬度
+        /// </summary>
+        void Refresh()
+        {
+            GridLength fooGridLength;
+            double fooValue = 0;
+            fooValue = OriginalHeight + (OffsetTotal_dy + OffsetMoving_dy);
+            fooGridLength = new GridLength(fooValue);
+            fooMainPageViewModel.RowTop = fooGridLength;
+            fooValue = OriginalHeight - (OffsetTotal_dy + OffsetMoving_dy);
+            fooGridLength = new GridLength(fooValue);
+            fooMainPageViewModel.RowBottom = fooGridLength;
+
+            fooValue = OriginWidth + (OffsetTotal_dx + OffsetMoving_dx);
+            fooGridLength = new GridLength(fooValue);
+            fooMainPageViewModel.ColumnLeft = fooGridLength;
+            fooValue = OriginWidth - (OffsetTotal_dx + OffsetMoving_dx);
+            fooGridLength = new GridLength(fooValue);
+            fooMainPageViewModel.ColumnRight = fooGridLength;
+        }
+
+        void Init()
+        {
+            GridLength fooGridLength;
+            OriginalHeight = (ImageHeight - fooMainPageViewModel.CenterHeight.Value) / 2;
+            OriginWidth = (ImageWidth - fooMainPageViewModel.CenterWidth.Value) / 2;
+            fooGridLength = new GridLength(OriginalHeight);
+            fooMainPageViewModel.RowTop = fooGridLength;
+            fooMainPageViewModel.RowBottom = fooGridLength;
+            fooGridLength = new GridLength(OriginWidth);
+            fooMainPageViewModel.ColumnLeft = fooGridLength;
+            fooMainPageViewModel.ColumnRight = fooGridLength;
+
+            OffsetMoving_dx = 0;
+            OffsetMoving_dy = 0;
+            OffsetTotal_dx = 0;
+            OffsetTotal_dy = 0;
+        }
+    }
+}
